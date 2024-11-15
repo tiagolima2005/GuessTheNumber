e5fbcad5880339ebf18210ac78dd101ba91c49b5
tiagolima2005 <tilima.steam@gmail.com>
2024-11-15 19:44:52 +0000

    added difficulty

--- GuessTheNumber.c
+++ GuessTheNumber.c
@@ -5,16 +5,38 @@
 int main()
 {
     int answer;
-    int upper_bound = 10;
+    int upper_bound;
     int lower_bound = 0;
     int attempts = 0;
-
-    srand(time(0));
-    int value = rand() % (upper_bound - lower_bound + 1) + lower_bound;
+    int difficulty;
 
     printf("Welcome to the 'Guess the Number'\n");
     printf("Rules are simple, type a number and see if its the chosen number or not\n");
     printf("Good Luck!\n");
+    printf("Choose the difficulty level:\n1 - Easy (0-10)\n2 - Medium (0-50)\n3 - Hard (0-100)\n");
+    scanf("%d", &difficulty);
+
+    if (difficulty == 1)
+    {
+        upper_bound = 10;
+    }
+    else if (difficulty == 2)
+    {
+        upper_bound = 50;
+    }
+    else if (difficulty == 3)
+    {
+        upper_bound = 100;
+    }
+    else
+    {
+        printf("Invalid number. Defaulting to Easy Difficulty (0-10)");
+        upper_bound = 10;
+    }
+
+    srand(time(0));
+    int value = rand() % (upper_bound - lower_bound + 1) + lower_bound;
+
     printf("Insert your number:\n");
     scanf("%d", &answer);
 
@@ -34,7 +56,7 @@ int main()
         attempts++;
     }
 
-    printf("Congratulations, you found the secret number in %d attempts!\n" , attempts);
+    printf("Congratulations, you found the secret number in %d attempts!\n", attempts);
 
     return 0;
 }
